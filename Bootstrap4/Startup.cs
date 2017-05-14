6858672828201f015dfe249344c756fab6ce28a1
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-05-14 20:22:53 +0200

    BS4 starter example
    - FontAwesome support
    - vendor inclusion via Yarn/NMP and custom static folder
    - markup from BS4

--- Bootstrap4/Startup.cs
+++ Bootstrap4/Startup.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace Bootstrap4
@@ -39,6 +42,14 @@ namespace Bootstrap4
             }
 
             app.UseStaticFiles();
+            // server vendor fiels from custom virtual directory to match /lib/{vendor}
+            // original paths in Razor markup from tempaltes
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                RequestPath = new PathString("/lib")
+            });
 
             app.UseMvc(routes =>
             {
