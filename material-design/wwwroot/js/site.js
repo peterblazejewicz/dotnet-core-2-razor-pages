084c01fc2a7e78d60c404498331d0cb57917ca63
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-05-15 23:11:14 +0200

    Sample with MD and drawer navigation

--- material-design/wwwroot/js/site.js
+++ material-design/wwwroot/js/site.js
@@ -1,1 +1,15 @@
 ﻿// Write your Javascript code.
+var drawerEl = document.querySelector('.mdc-temporary-drawer');
+var MDCTemporaryDrawer = mdc.drawer.MDCTemporaryDrawer;
+var drawer = new MDCTemporaryDrawer(drawerEl);
+document
+    .querySelector('.demo-menu')
+    .addEventListener('click', function () {
+        drawer.open = true;
+    });
+drawerEl.addEventListener('MDCTemporaryDrawer:open', function () {
+    console.log('Received MDCTemporaryDrawer:open');
+});
+drawerEl.addEventListener('MDCTemporaryDrawer:close', function () {
+    console.log('Received MDCTemporaryDrawer:close');
+});
