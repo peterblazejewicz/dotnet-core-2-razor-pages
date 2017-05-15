ba362aa73189cc49fce7034dd5d1ddcc2a05e92e
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-05-15 22:52:08 +0200

    Cleanup code for MD sample

--- material-design/Startup.cs
+++ material-design/Startup.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace MaterialDesignSample
@@ -39,6 +42,12 @@ namespace MaterialDesignSample
             }
 
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                RequestPath = new PathString("/lib")
+            });
 
             app.UseMvc(routes =>
             {
